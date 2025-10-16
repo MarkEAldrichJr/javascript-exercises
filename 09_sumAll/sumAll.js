5ab4fd43bc2922b650321160dbbfae3d58ddb47e
MarkEAldrichJr <44414229+MarkEAldrichJr@users.noreply.github.com>
2025-10-16 15:57:39 -0400

    Completed sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b))
+        return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
 
+    let val = 0;
+    if (a > b){
+        for (let i = b; i <= a; i++){
+            val += i;
+        }
+    } else if (a < b) {
+        for (let i = a; i <= b; i++){
+            val += i;
+        }
+    }
+    return val;
 };
 
 // Do not edit below this line
